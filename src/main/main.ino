50b86565da30af86744c9b8ebd7a2d247a986a66
Peter Mačinec <peter.macinec@gmail.com>
2020-11-14 21:04:14 +0100

    Reformat code according to standards

--- src/main/main.ino
+++ src/main/main.ino
@@ -24,7 +24,8 @@ int screenWidth = 128;
  * @param text Text to be printed.
  * @param positionY Height (y) position to print text at.
  */
-void printCenteredText(String text, int positionY) {
+void printCenteredText(String text, int positionY)
+{
   u8g.setPrintPos((screenWidth - u8g.getStrPixelWidth(text.c_str())) / 2, positionY);
   u8g.print(text);
 }
@@ -32,22 +33,26 @@ void printCenteredText(String text, int positionY) {
 /**
  * Print device intro page.
  */
-void printIntroPage() {
+void printIntroPage()
+{
   u8g.firstPage();
-  do {
+  do
+  {
     u8g.setFont(u8g_font_profont15);
     printCenteredText("Programmer", 20);
     printCenteredText("reminder", 40);
-  } while(u8g.nextPage());
+  } while (u8g.nextPage());
   delay(introScreenDuration * 1000);
 }
 
 /**
  * Print menu page.
  */
-void printMenuPage(void) {
+void printMenuPage(void)
+{
   u8g.firstPage();
-  do {
+  do
+  {
     u8g.setFont(u8g_font_profont12);
     u8g.setPrintPos(0, 10);
     u8g.print("Please, choose");
@@ -64,9 +69,11 @@ void printMenuPage(void) {
 /**
  * Print Pomodoro intro page.
  */
-void printPomodoroIntroPage() {
+void printPomodoroIntroPage()
+{
   u8g.firstPage();
-  do {
+  do
+  {
     u8g.setFont(u8g_font_profont15);
     printCenteredText("Pomodoro", 35);
   } while (u8g.nextPage());
@@ -76,11 +83,13 @@ void printPomodoroIntroPage() {
 /**
  * Print Pomodoro intro page.
  */
-void printBasicWorkModeIntroPage() {
+void printBasicWorkModeIntroPage()
+{
   u8g.firstPage();
-  do {
-    u8g.setFont(u8g_font_profont12);  
-    printCenteredText("Wish you productive", 25); 
+  do
+  {
+    u8g.setFont(u8g_font_profont12);
+    printCenteredText("Wish you productive", 25);
     printCenteredText("day at work!", 50);
   } while (u8g.nextPage());
   delay(basicWorkModeIntroScreenDuration * 1000);
@@ -92,7 +101,8 @@ void printBasicWorkModeIntroPage() {
  * @param buttonPin Pin at which button is binded.
  * @returns Bool value whether was button pushed or not.
  */
-bool wasButtonPushed(int buttonPin) {
+bool wasButtonPushed(int buttonPin)
+{
   int buttonState = digitalRead(buttonPin);
   return buttonState == HIGH;
 }
@@ -102,17 +112,21 @@ bool wasButtonPushed(int buttonPin) {
  * 
  * @param text Text to print during alarm.
  */
-void alarm(String text) {
+void alarm(String text)
+{
   u8g.firstPage();
-  do {
+  do
+  {
     u8g.setFont(u8g_font_profont12);
     printCenteredText(text, 30);
     u8g.setFont(u8g_font_profont10);
     printCenteredText("Stop with GREEN button", 45);
   } while (u8g.nextPage());
 
-  while (true) {
-    if (wasButtonPushed(stopBuzzerButton)) return;
+  while (true)
+  {
+    if (wasButtonPushed(stopBuzzerButton))
+      return;
 
     tone(buzzer, NOTE_E4, 0.5);
     delay(100);
@@ -128,21 +142,23 @@ void alarm(String text) {
  * @param hideHours Whether to hide hours or not (only MM:SS string will be returned).
  * @returns Time string in HH:MM:SS format.
  */
-String getTimeString(int secondsElapsed, bool hideHours = false) {
+String getTimeString(int secondsElapsed, bool hideHours = false)
+{
   int hours = secondsElapsed / 3600;
   int minutes = secondsElapsed / 60;
   int seconds = secondsElapsed % 60;
 
   String timeString = "";
 
-  if (!hideHours) {
+  if (!hideHours)
+  {
     timeString += (hours < 10) ? "0" + String(hours) : String(hours);
     timeString += ":";
   }
   timeString += (minutes < 10) ? "0" + String(minutes) : String(minutes);
   timeString += ":";
   timeString += (seconds < 10) ? "0" + String(seconds) : String(seconds);
-  
+
   return timeString;
 }
 
@@ -153,7 +169,8 @@ String getTimeString(int secondsElapsed, bool hideHours = false) {
  * @param inerruptPin Pin that can interrupt timer.
  * @returns Whether timer ended normally (0) or by interruption (1);
  */
-int timer(int secs, int interruptPin) {
+int timer(int secs, int interruptPin)
+{
   unsigned long startTime = millis();
 
   int secondsPassed = 0;
@@ -162,17 +179,20 @@ int timer(int secs, int interruptPin) {
   int secondsRemaining = secs % 60;
   String timeToPrint;
 
-  while (secondsPassed <= secs) {
+  while (secondsPassed <= secs)
+  {
     timeToPrint = getTimeString(secs - secondsPassed, true);
     u8g.firstPage();
-    do {
+    do
+    {
       u8g.setFont(u8g_font_profont15);
       printCenteredText(timeToPrint, 35);
     } while (u8g.nextPage());
-  
+
     secondsPassed = (millis() - startTime) / 1000;
 
-    if (wasButtonPushed(interruptPin)) return 1;
+    if (wasButtonPushed(interruptPin))
+      return 1;
   }
   return 0;
 }
@@ -180,27 +200,35 @@ int timer(int secs, int interruptPin) {
 /**
  * Pomodoro timer functionality.
  */
-void pomodoroTimer() {
+void pomodoroTimer()
+{
   int timerResponse = 0;
 
-  while (true) {
-    if (timerResponse == 1) return;
+  while (true)
+  {
+    if (timerResponse == 1)
+      return;
 
-    for (int pomodoroLoops = 0; pomodoroLoops <= pomodoroWorkLoops - 1; pomodoroLoops++) {
+    for (int pomodoroLoops = 0; pomodoroLoops <= pomodoroWorkLoops - 1; pomodoroLoops++)
+    {
       alarm("Work!");
       timerResponse = timer(pomodoroWorkDuration, pomodoroModeButton);
-      if (timerResponse == 1) return;
+      if (timerResponse == 1)
+        return;
 
-      if (pomodoroLoops < 3) {
+      if (pomodoroLoops < 3)
+      {
         alarm("Short rest");
         timerResponse = timer(pomodoroShortRestDuration, pomodoroModeButton);
-        if (timerResponse == 1) return;
+        if (timerResponse == 1)
+          return;
       }
     }
 
     alarm("Long rest");
     timerResponse = timer(pomodoroLongRestDuration, pomodoroModeButton);
-    if (timerResponse == 1) return;
+    if (timerResponse == 1)
+      return;
   }
 }
 
@@ -211,7 +239,8 @@ void pomodoroTimer() {
  * @param count Whether to count worked time or remain static.
  * @returns Whether screen print ended normally (0) or by interruption (1);
  */
-int printWorkedTime(int workedSeconds, bool count = true) {
+int printWorkedTime(int workedSeconds, bool count = true)
+{
   unsigned long startTime = millis();
 
   int secondsPassed = 0;
@@ -219,24 +248,29 @@ int printWorkedTime(int workedSeconds, bool count = true) {
   String timeToPrint;
   String workedTimeMessage = count ? "Worked time" : "Worked time today";
 
-  while (secondsPassed <= basicWorkModeScreensChange) {
+  while (secondsPassed <= basicWorkModeScreensChange)
+  {
     timeToPrint = getTimeString(workedSeconds + secondsPassed);
     u8g.firstPage();
-    do {
+    do
+    {
       u8g.setFont(u8g_font_profont15);
-      
+
       printCenteredText(workedTimeMessage, 20);
       printCenteredText(timeToPrint, 40);
     } while (u8g.nextPage());
- 
-    if (!count) {
+
+    if (!count)
+    {
       return 0;
     }
-    else {
+    else
+    {
       secondsPassed = (millis() - startTime) / 1000;
     }
 
-    if (wasButtonPushed(basicModeButton)) return 1;
+    if (wasButtonPushed(basicModeButton))
+      return 1;
   }
 
   return 0;
@@ -250,11 +284,13 @@ int printWorkedTime(int workedSeconds, bool count = true) {
  * @param humidity Relative humidity percentage.
  * @returns Whether screen print ended normally (0) or by interruption (1);
  */
-int printEnvironmentMeasuredValues(float illuminance, float temperature, float humidity) {
+int printEnvironmentMeasuredValues(float illuminance, float temperature, float humidity)
+{
   unsigned long startTime = millis();
 
   u8g.firstPage();
-  do {
+  do
+  {
     u8g.setFont(u8g_font_profont15);
     u8g.drawBitmapP(0, 0, 2, 16, illuminanceBitmap);
     u8g.setPrintPos(25, 12);
@@ -270,8 +306,10 @@ int printEnvironmentMeasuredValues(float illuminance, float temperature, float h
     u8g.print(String(humidity) + " rH%");
   } while (u8g.nextPage());
 
-  while ((millis() - startTime) / 1000 <= basicWorkModeScreensChange) {
-    if (wasButtonPushed(basicModeButton)) return 1;
+  while ((millis() - startTime) / 1000 <= basicWorkModeScreensChange)
+  {
+    if (wasButtonPushed(basicModeButton))
+      return 1;
   }
 
   return 0;
@@ -284,24 +322,30 @@ int printEnvironmentMeasuredValues(float illuminance, float temperature, float h
  * @param temperature Temperature in degree Celsius.
  * @param humidity Relative humidity percentage.
  */
-void checkEnvironmentMeasuredValues(float illuminance, float temperature, float humidity) {
-  if (illuminance < illuminanceLowerBoundary) {
+void checkEnvironmentMeasuredValues(float illuminance, float temperature, float humidity)
+{
+  if (illuminance < illuminanceLowerBoundary)
+  {
     alarm("Illuminance too low!");
   }
 
-  if (temperature <= temperatureLowerBoundary) {
+  if (temperature <= temperatureLowerBoundary)
+  {
     alarm("Temperature too low!");
   }
 
-  if (temperature >= temperatureUpperBoundary) {
+  if (temperature >= temperatureUpperBoundary)
+  {
     alarm("Temperature too high!");
   }
 
-  if (humidity <= humidityLowerBoundary) {
+  if (humidity <= humidityLowerBoundary)
+  {
     alarm("Humidity too low!");
   }
 
-  if (humidity >= humidityUpperBoundary) {
+  if (humidity >= humidityUpperBoundary)
+  {
     alarm("Humidity too high!");
   }
 }
@@ -311,11 +355,13 @@ void checkEnvironmentMeasuredValues(float illuminance, float temperature, float 
  * 
  * @returns Whether screen print ended normally (0) or by interruption (1);
  */
-int printEnvironmentIdealValues() {
+int printEnvironmentIdealValues()
+{
   unsigned long startTime = millis();
 
   u8g.firstPage();
-  do {
+  do
+  {
     u8g.setFont(u8g_font_profont15);
     u8g.drawBitmapP(0, 0, 2, 16, illuminanceBitmap);
     u8g.setPrintPos(23, 12);
@@ -331,8 +377,10 @@ int printEnvironmentIdealValues() {
     u8g.print(String(humidityLowerBoundary) + "-" + String(humidityUpperBoundary) + " rH%");
   } while (u8g.nextPage());
 
-  while ((millis() - startTime) / 1000 <= basicWorkModeScreensChange) {
-    if (wasButtonPushed(basicModeButton)) return 1;
+  while ((millis() - startTime) / 1000 <= basicWorkModeScreensChange)
+  {
+    if (wasButtonPushed(basicModeButton))
+      return 1;
   }
 
   return 0;
@@ -343,7 +391,8 @@ int printEnvironmentIdealValues() {
  * 
  * Code taken (and modified) from https://github.com/robsoncouto/arduino-songs.
  */
-void playSong() {
+void playSong()
+{
   // Sizeof gives the number of bytes, each int value is composed of two bytes (16 bits),
   // there are two values per note (pitch and duration), so for each note there are four bytes
   int notes = sizeof(melody) / sizeof(melody[0]) / 2;
@@ -354,15 +403,20 @@ void playSong() {
   int divider = 0, noteDuration = 0;
 
   // Remember, the array is twice the number of notes (notes + durations)
-  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
-    if (wasButtonPushed(basicModeButton)) return;
+  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2)
+  {
+    if (wasButtonPushed(basicModeButton))
+      return;
 
     // Calculates the duration of each note
     divider = melody[thisNote + 1];
-    if (divider > 0) {
+    if (divider > 0)
+    {
       // Regular note, just proceed
       noteDuration = (wholenote) / divider;
-    } else if (divider < 0) {
+    }
+    else if (divider < 0)
+    {
       // Dotted notes are represented with negative durations
       noteDuration = (wholenote) / abs(divider);
       noteDuration *= 1.5; // Increases the duration in half for dotted notes
@@ -373,7 +427,7 @@ void playSong() {
 
     // Wait for the specief duration before playing the next note.
     delay(noteDuration);
-    
+
     // Stop the waveform generation before the next note.
     noTone(buzzer);
   }
@@ -384,7 +438,8 @@ void playSong() {
  * 
  * @param workedSeconds Worked time in seconds.
  */
-void finalizeBasicWorkingMode(int workedSeconds) {
+void finalizeBasicWorkingMode(int workedSeconds)
+{
   printWorkedTime(workedSeconds, false);
   delay(100);
   playSong();
@@ -393,7 +448,8 @@ void finalizeBasicWorkingMode(int workedSeconds) {
 /**
  * Start basic work mode.
  */
-void basicWorkMode() {
+void basicWorkMode()
+{
   int workStart = millis();
 
   int returnState = 0;
@@ -407,34 +463,41 @@ void basicWorkMode() {
   float temperature;
   float humidity;
 
-  while (true) {
+  while (true)
+  {
     illuminance = lightMeter.readLightLevel();
     temperature = bme.temp();
     humidity = bme.hum();
     returnState = printEnvironmentMeasuredValues(illuminance, temperature, humidity);
-    if (returnState == 1) break;
+    if (returnState == 1)
+      break;
 
     checkEnvironmentMeasuredValues(illuminance, temperature, humidity);
 
     returnState = printEnvironmentIdealValues();
-    if (returnState == 1) break;
+    if (returnState == 1)
+      break;
 
     returnState = printWorkedTime((millis() - workStart) / 1000);
-    if (returnState == 1) break;
+    if (returnState == 1)
+      break;
 
-    if ((millis() - eyesRestTimeTimer) / 1000 + basicWorkModeScreensChange * 3 / 2 >= eyesRestTime) {
+    if ((millis() - eyesRestTimeTimer) / 1000 + basicWorkModeScreensChange * 3 / 2 >= eyesRestTime)
+    {
       alarm("Eyes rest!");
       timer(eyesRestDelay, basicModeButton);
       eyesRestTimeTimer = millis();
     }
 
-    if ((millis() - waterDrinkingRestTimeTimer) / 1000 + basicWorkModeScreensChange * 3 / 2 >= waterDrinkingRestTime) {
+    if ((millis() - waterDrinkingRestTimeTimer) / 1000 + basicWorkModeScreensChange * 3 / 2 >= waterDrinkingRestTime)
+    {
       alarm("Drink water!");
       timer(waterDrinkingRestDelay, basicModeButton);
       waterDrinkingRestTimeTimer = millis();
     }
 
-    if ((millis() - exerciseRestTimeTimer) / 1000 + basicWorkModeScreensChange * 3 / 2 >= exerciseRestTime) {
+    if ((millis() - exerciseRestTimeTimer) / 1000 + basicWorkModeScreensChange * 3 / 2 >= exerciseRestTime)
+    {
       alarm("Exercise!");
       timer(exerciseRestDelay, basicModeButton);
       exerciseRestTimeTimer = millis();
@@ -444,7 +507,8 @@ void basicWorkMode() {
   finalizeBasicWorkingMode((millis() - workStart) / 1000);
 }
 
-void setup() {
+void setup()
+{
   // Initialize input/ouput modules pins
   pinMode(basicModeButton, INPUT);
   pinMode(pomodoroModeButton, INPUT);
@@ -457,15 +521,18 @@ void setup() {
   printIntroPage();
 }
 
-void loop() {
+void loop()
+{
   printMenuPage();
- 
-  if (wasButtonPushed(basicModeButton)) {
+
+  if (wasButtonPushed(basicModeButton))
+  {
     printBasicWorkModeIntroPage();
     basicWorkMode();
   }
 
-  if (wasButtonPushed(pomodoroModeButton)) {
+  if (wasButtonPushed(pomodoroModeButton))
+  {
     printPomodoroIntroPage();
     pomodoroTimer();
   }
